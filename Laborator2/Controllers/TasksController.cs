b262738a16d9d3397f14729ebef1717f35834522
agtourcj3 <lucian.maniu@gmail.com>
2019-05-12 10:12:27 +0300

    Initial commit - created database

---
+++ Laborator2/Controllers/TasksController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Laborator2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Laborator2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private TasksDbContext context;
+        public TasksController(TasksDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public IEnumerable<Models.Task> Get([FromQuery]DateTime? deadlineFrom, [FromQuery]DateTime? deadlineTo)
+        {
+            IQueryable<Models.Task> result = context.Tasks.Include(t => t.Comments);
+
+            if (deadlineFrom == null && deadlineTo == null)
+                return result;
+
+            if (deadlineFrom != null)
+                result = result.Where(t => t.Deadline >= deadlineFrom);
+
+            if (deadlineTo != null)
+                result = result.Where(t => t.Deadline <= deadlineTo);
+
+            return result;
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var existing = context.Tasks.Include(t => t.Comments).FirstOrDefault(task => task.Id == id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(existing);
+        }
+
+        // POST: api/Tasks
+        [HttpPost]
+        public void Post([FromBody] Models.Task task)
+        {
+            task.DateClosed = null;
+            task.DateAdded = DateTime.Now;
+            context.Tasks.Add(task);
+            context.SaveChanges();
+        }
+
+        // PUT: api/Tasks/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Models.Task task)
+        {
+            var existing = context.Tasks.AsNoTracking().FirstOrDefault(t => t.Id == id);
+            if (existing == null)
+            {
+                task.DateClosed = null;
+                task.DateAdded = DateTime.Now;
+                context.Tasks.Add(task);
+                context.SaveChanges();
+                return Ok(task);
+            }
+            task.Id = id;
+            if (task.TaskState == TaskState.Closed && existing.TaskState != TaskState.Closed)
+                task.DateClosed = DateTime.Now;
+            else if (existing.TaskState == TaskState.Closed && task.TaskState != TaskState.Closed)
+                task.DateClosed = null;
+
+            context.Tasks.Update(task);
+            context.SaveChanges();
+            return Ok(task);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existing = context.Tasks.FirstOrDefault(t => t.Id == id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+            context.Tasks.Remove(existing);
+            context.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ Laborator2/Models/TasksDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laborator2.Models
+{
+    // DbContext = Unit of Work
+    public class TasksDbContext : DbContext
+    {
+        public TasksDbContext(DbContextOptions<TasksDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet = Repository
+        // DbSet = O tabela din baza de date
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbContextOptions<TasksDbContext> Options { get; }
+    }
+}
